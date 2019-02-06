fab5de20f051da949c7b2b5cb8ade0ecf11c8eab
Le Minh Tri <ansidev@gmail.com>
2019-02-06 20:40:59 +0700

    Update site config

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -2,20 +2,47 @@ module.exports = {
   title: 'Christian Ebook Collection',
   description: '🎉 A collection of Christian e-book',
   head: [
-    ['link', { rel: 'icon', href: `/logo.png` }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
-    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#fff' }]
+    ['link', {
+      rel: 'icon',
+      href: `/logo.png`
+    }],
+    ['link', {
+      rel: 'manifest',
+      href: '/manifest.json'
+    }],
+    ['meta', {
+      name: 'theme-color',
+      content: '#006df0'
+    }],
+    ['meta', {
+      name: 'apple-mobile-web-app-capable',
+      content: 'yes'
+    }],
+    ['meta', {
+      name: 'apple-mobile-web-app-status-bar-style',
+      content: 'black'
+    }],
+    ['link', {
+      rel: 'apple-touch-icon',
+      href: '/icons/ios/ios-appicon-152-152.png'
+    }],
+    ['link', {
+      rel: 'mask-icon',
+      href: '/icons/ios/ios-appicon-180-180.png',
+      color: '#006df0'
+    }],
+    ['meta', {
+      name: 'msapplication-TileImage',
+      content: '/icons/windows10/Square150x150Logo.scale-150.png'
+    }],
+    ['meta', {
+      name: 'msapplication-TileColor',
+      content: '#fff'
+    }]
   ],
   serviceWorker: true,
   base: '/',
   ga: 'UA-108003633-2',
-  theme: '@safestudio/vuepress-theme-ebook',
   themeConfig: {
     repo: 'SafeStudio/christian-ebook-collection',
     editLinks: true,
@@ -24,15 +51,15 @@ module.exports = {
     serviceWorker: {
       updatePopup: true,
     },
-    sidebar: [
-      {
-        title: 'Dưỡng linh',
-        collapsable: false,
-        children: [
-          '/books/duong-linh/binh-an-voi-chua',
-        ],
-      },
-    ]
+    sidebar: [{
+      title: 'Dưỡng linh',
+      collapsable: false,
+      type: 'book_collection',
+      basePath: '/books/duong-linh/',
+      books: [
+        'binh-an-voi-chua',
+      ],
+    }, ],
   },
   plugins: [
     '@vuepress/back-to-top',
