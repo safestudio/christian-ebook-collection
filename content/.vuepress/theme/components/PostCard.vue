6f4728633a6fd601fdcc536627c67ed2c2415a06
Le Minh Tri <ansidev@gmail.com>
2019-02-13 04:27:06 +0700

    Custom theme

---
+++ content/.vuepress/theme/components/PostCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card">
+    <img v-if="postCoverImage" :src="$withBase(postCoverImage)" class="card-img-top" alt="">
+    <div class="card-body">
+      <h3 class="card-title">
+        <router-link :to="post.path">{{post.frontmatter.title}}</router-link>
+      </h3>
+      <p class="card-text">{{post.frontmatter.description}}</p>
+      <p class="card-text">{{post.frontmatter.author}}</p>
+    </div>
+    <div class="card-body">
+      <tag v-for="tag in post.frontmatter.tags"
+        :key="tag"
+        :slug="tag">
+        {{tag}}
+      </tag>
+    </div>
+  </div>
+</template>
+
+<script>
+import Tag from './Tag';
+
+export default {
+  name: 'post-card',
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  },
+  components: { Tag },
+  computed: {
+    postCoverImage() {
+      return this.post.frontmatter.coverImage;
+    }
+  }
+};
+</script>
---
+++ content/.vuepress/theme/components/Tag.vue
@@ -0,0 +1,25 @@
+<template>
+  <span :class="['tag', `tag-${tagStyle}`, 'mx-1']">
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+const tagStyles = [
+  'primary',
+  'success',
+  'info',
+  'warning',
+  'danger',
+  'light',
+  'dark',
+]
+export default {
+  props: ['tag', 'slug'],
+  computed: {
+    tagStyle() {
+      return tagStyles[Math.floor(Math.random() * tagStyles.length)]
+    }
+  }
+};
+</script>
